a25b42ce4e4db0866f96a1d2fe7ae4b99e281b0c
RedCrusaderJr <dimitrijemitic1996@uns.ac.rs>
2019-01-04 20:21:57 +0100

    Delim ekran na game_bord_frame i stat_frame

--- main_window.py
+++ main_window.py
@@ -1,7 +1,35 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QAction, QMenu, QSplitter, QHBoxLayout, QVBoxLayout, QFrame
+from PyQt5.QtCore import Qt
 from game_board import GameBoard
+from stat_frame import StatFrame
 import sys
 #from level import Level
+
+class FormWidget(QWidget):
+
+    def __init__(self, parent):
+        super(FormWidget, self).__init__(parent)
+        self.layout = QHBoxLayout(self)
+
+        self.game_board_frame = GameBoard(self)
+        self.game_board_frame.setMinimumSize(1000, 562.5)
+        self.game_board_frame.setMaximumSize(1000, 562.5)
+        self.game_board_frame.setObjectName("game_board_frame")
+        self.layout.addWidget(self.game_board_frame)
+
+        self.stat_frame = StatFrame(self)
+        self.stat_frame.setMaximumHeight(562.5)
+        self.stat_frame.setObjectName("stat_frame")
+        self.layout.addWidget(self.stat_frame)
+
+        self.setLayout(self.layout)
+        # self.game_board_frame.setFrameShape(QFrame.StyledPanel)
+
+        # self.stat_frame.setFrameShape(QFrame.StyledPanel)
+
+        # self.splitter = QSplitter(Qt.Vertical)
+        # self.splitter.addWidget(self.game_board_frame)
+        # self.splitter.addWidget(self.stat_frame)
 
 class BattleCity(QMainWindow):
 
@@ -11,20 +39,56 @@ class BattleCity(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.showFullScreen()
-        self.game_board = GameBoard(self)
-        self.setCentralWidget(self.game_board)
+        #self.setMaximumSize(1920, 1080)
+        self.setMinimumSize(1500, 700)
+
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage("Ready")
+
+        menu_bar = self.menuBar()
+        settings_menu = menu_bar.addMenu("&Settings")
+
+        mode_menu = QMenu("Mode", self)
+
+        single_act = QAction("Single paleyer", self)
+        single_act.triggered.connect(self.toggleSingle)
+        multi_act = QAction('Multipaleyer mode', self)
+        multi_act.triggered.connect(self.toggleMulti)
+
+        mode_menu.addAction(single_act)
+        mode_menu.addAction(multi_act)
+
+        start_act = QAction('Start game', self)
+        start_act.triggered.connect(self.startGame)
+
+        settings_menu.addMenu(mode_menu)
+        settings_menu.addAction(start_act)
+
+        self.show()
+
+    def startGame(self):
+        self.form_widget = FormWidget(self)
+        self.setCentralWidget(self.form_widget)
+
+        #self.setCentralWidget(self.game_board)
         self.setStyleSheet("""
-        QFrame{
-            background-color: rgb(0, 0, 0);
-            }
+                QFrame#game_board_frame{
+                    background-color: rgb(0, 0, 0);
+                    }
+                QFrame#stat_frame{
+                    background-color: rgb(255, 0, 0);
+                    }
         """)
         self.show()
 
+    def toggleSingle(self, mode):
+        self.status_bar.showMessage("MODE: SINGLE PLAYER")
+
+    def toggleMulti(self, mode):
+        self.status_bar.showMessage("MODE: MULTIPLAYER")
 
 if __name__ == "__main__":
     app = QApplication([])
 
     battleCity = BattleCity()
     sys.exit(app.exec_())
-
